4ab4a9f0f3c6d6af0c4ca9e1e16d9d4d04902651
Tahir-07 <tahirmulani7447@gmail.com>
2026-07-14 14:42:02 +0530

    simple example practice

---
+++ TestApp.js
@@ -0,0 +1,8 @@
+const num = 42;
+const numObj = new Number("42");
+
+console.log(numObj.valueOf());  	
+console.log(typeof numObj);
+console.log(typeof numObj.valueOf());
+console.log(a);
+const a = 10;
